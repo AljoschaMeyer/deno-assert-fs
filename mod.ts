0d4d33f4ab5e1bdbc16e1d571a4c6353e9a4988b
Aljoscha Meyer <mail@aljoscha-meyer.de>
2024-02-20 14:57:23 +0100

    Fix some mistakes

--- mod.ts
+++ mod.ts
@@ -1,12 +1,69 @@
 import { Colors } from "./deps.ts";
 import { fail, fs, join } from "./deps.ts";
 
-export function assertFs(gotPath: string, expectedPath: string) {
-  assertAsymmetric(gotPath, expectedPath, true);
-  assertAsymmetric(expectedPath, gotPath, false);
+export async function assertFs(gotPath: string, expectedPath: string) {
+  await assertAsymmetric(gotPath, expectedPath, true);
+  await assertAsymmetric(expectedPath, gotPath, false);
 }
 
-export function assertAsymmetric(
+async function assertAsymmetric(
+  p1: string,
+  p2: string,
+  firstArgIsGot: boolean,
+) {
+  if (!(await Deno.stat(p1)).isDirectory) {
+    fail(`Expected ${Colors.cyan(p1)} to be a directory.`);
+  }
+  if (!(await Deno.stat(p2)).isDirectory) {
+    fail(`Expected ${Colors.cyan(p2)} to be a directory.`);
+  }
+
+  for await (
+    const entry of fs.walk(p1, {
+      includeDirs: true,
+      includeFiles: true,
+    })
+  ) {
+    const p = entry.path;
+    const suffix =  p.slice(p1.length - 1);
+    const otherP = join(p2, suffix);
+    const otherStats = await Deno.stat(otherP);
+
+    const styledP = firstArgIsGot ? Colors.red(p) : Colors.green(p);
+    const styledOtherP = firstArgIsGot ? Colors.green(p) : Colors.red(p);
+
+    if (entry.isDirectory && !otherStats.isDirectory) {
+      fail(
+        `Mismatching directories: ${styledP} is a directory, but ${styledOtherP} is not.`,
+      );
+    } else if (entry.isFile && !otherStats.isFile) {
+      fail(
+        `Mismatching file types: ${styledP} is a file, but ${styledOtherP} is not.`,
+      );
+    } else if (entry.isFile && otherStats.isFile) {
+      const pContent = await Deno.readTextFile(p);
+      const otherPContent = await Deno.readTextFile(otherP);
+
+      if (pContent != otherPContent) {
+        fail(`Nonequal file contents at ${styledOtherP} and ${styledP}.
+
+${styledOtherP}:
+${Colors.red(otherPContent)}
+----====-----=====------======-----=====----====----
+${styledP}:
+${Colors.green(pContent)}`);
+      }
+    }
+  }
+}
+
+
+export function assertFsSync(gotPath: string, expectedPath: string) {
+  assertAsymmetricSync(gotPath, expectedPath, true);
+  assertAsymmetricSync(expectedPath, gotPath, false);
+}
+
+function assertAsymmetricSync(
   p1: string,
   p2: string,
   firstArgIsGot: boolean,
@@ -25,7 +82,7 @@ export function assertAsymmetric(
     })
   ) {
     const p = entry.path;
-    const suffix = p.slice(p1.length);
+    const suffix = p.slice(p1.length - 1);
     const otherP = join(p2, suffix);
     const otherStats = Deno.statSync(otherP);
 
--- test/test.ts
+++ test/test.ts
@@ -1,21 +1,40 @@
-import { assertThrows } from "https://deno.land/std@0.215.0/assert/mod.ts";
-import {assertFs} from "../mod.ts";
+import { assertThrows, assertRejects } from "https://deno.land/std@0.215.0/assert/mod.ts";
+import {assertFsSync, assertFs } from "../mod.ts";
+
+Deno.test("equality", async () => {
+  await assertFs("dirA", "dirB");
+});
+
+Deno.test("equality symmetric", async () => {
+  await assertFs("dirB", "dirA");
+});
+
+Deno.test("nonequal directories", async () => {
+  await assertRejects(async () => await assertFs("dirA", "dirC"));
+  await assertRejects(async () => await assertFs("dirA", "dirD"));
+  await assertRejects(async () => await assertFs("dirA", "dirE"));
+  await assertRejects(async () => await assertFs("dirA", "dirF"));
+  await assertRejects(async () => await assertFs("dirC", "dirA"));
+  await assertRejects(async () => await assertFs("dirD", "dirA"));
+  await assertRejects(async () => await assertFs("dirE", "dirA"));
+  await assertRejects(async () => await assertFs("dirF", "dirA"));
+});
 
 Deno.test("equality", () => {
-  assertFs("dirA", "dirB");
+  assertFsSync("dirA", "dirB");
 });
 
 Deno.test("equality symmetric", () => {
-  assertFs("dirB", "dirA");
+  assertFsSync("dirB", "dirA");
 });
 
 Deno.test("nonequal directories", () => {
-  assertThrows(() => assertFs("dirA", "dirC"));
-  assertThrows(() => assertFs("dirA", "dirD"));
-  assertThrows(() => assertFs("dirA", "dirE"));
-  assertThrows(() => assertFs("dirA", "dirF"));
-  assertThrows(() => assertFs("dirC", "dirA"));
-  assertThrows(() => assertFs("dirD", "dirA"));
-  assertThrows(() => assertFs("dirE", "dirA"));
-  assertThrows(() => assertFs("dirF", "dirA"));
+  assertThrows(() => assertFsSync("dirA", "dirC"));
+  assertThrows(() => assertFsSync("dirA", "dirD"));
+  assertThrows(() => assertFsSync("dirA", "dirE"));
+  assertThrows(() => assertFsSync("dirA", "dirF"));
+  assertThrows(() => assertFsSync("dirC", "dirA"));
+  assertThrows(() => assertFsSync("dirD", "dirA"));
+  assertThrows(() => assertFsSync("dirE", "dirA"));
+  assertThrows(() => assertFsSync("dirF", "dirA"));
 });
